9cacf9a46a592a9866c85d55e365c814b6cf45c6
Grampurohit <vijayendra.grampurohit@honeywell.com>
2017-11-11 17:03:27 +0530

    script to convert to transparent image

---
+++ transperant_image.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+folder_path_input = "C:/Users/H213561/Documents/magnet/images-whiteBG/"
+folder_path_output= "C:/Users/H213561/Documents/magnet/images-transparentBG/"
+
+image_names =os.listdir(folder_path_input)
+
+for img_name in image_names:
+    print img_name
+    img = Image.open(folder_path_input+img_name)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+    #os.chdir(folder_path_output)
+    img.putdata(newData)
+    img.save(folder_path_output+img_name, "PNG")
